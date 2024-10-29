e9f93f05548f916a9bbadcee23026fea51831bb0
AIproblemsolving <automation.bot.as@gmail.com>
2024-10-29 22:30:36 +0100

    Update layout.py

--- layout.py
+++ layout.py
@@ -51,12 +51,19 @@ def hide_st():
         footer {visibility: hidden;}
         header {visibility: hidden;}
         
-        ._profileContainer_1yi6l_53 {display: none;}
-        ._profilePreview_51w34_63 {display: none;}
-        .stMain.st-emotion-cache-bm2z3a.ea3mdgi8 {display: none;}
+        ._profileContainer_1yi6l_53 visibility: hidden;}
+        ._profilePreview_51w34_63 visibility: hidden;}
+        .stMain.st-emotion-cache-bm2z3a.ea3mdgi8 visibility: hidden;}
         
-        div[class*="_profileContainer_1yi6l_53"] {display: none;}
-        div[class*="_profilePreview_51w34_63"] {display: none;}
+        div[class*="_profileContainer_1yi6l_53"] visibility: hidden;}
+        div[class*="_profilePreview_51w34_63"] visibility: hidden;}
+
+        img {display: none}
+        a {
+            pointer-events: none;
+            cursor: default;
+            color: inherit;
+        }
         
         </style>
         """
